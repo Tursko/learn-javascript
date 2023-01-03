4c4e605026b4edcd3ca7ab92894924cbb566f427
Tursko <taylor@stykocompany.com>
2023-01-03 05:45:17 -0600

    update and display count

--- Section-1/index.js
+++ Section-1/index.js
@@ -1,1 +1,13 @@
-document.getElementById("count-el").innerText = 5
+//document.getElementById("count-el").innerText = 5
+
+// ctrl + k + c
+
+let count = 0;
+let countEl = document.getElementById("count-el");
+console.log(countEl);
+
+function increment() {
+    count++;
+    console.log(count);
+    countEl.innerText = count;
+}
