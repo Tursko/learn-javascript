f72ecaae9f0370fe6f1476e0f0f907f322aeb11b
Tursko <taylor@stykocompany.com>
2023-01-07 14:30:27 -0600

    start building blackjack game

---
+++ Section-3/index.js
@@ -0,0 +1,24 @@
+// Backjack rules
+// less than 21 (good)
+// equal to 21 (win!)
+// greater than 21 (lose)
+
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard + 4;
+let hasBlackJack = false;
+let isAlive = true;
+
+let message = "";
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+} else if (sum === 21) {
+    hasBlackJack = true;
+    message = "BlackJack! You win!";
+} else {
+    isAlive = false;
+    message = "You lose!";
+}
+
+console.log(message);
