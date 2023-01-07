05d0b1c5ad3623c595e2653e8e14c27e3621d420
Tursko <taylor@stykocompany.com>
2023-01-07 14:54:09 -0600

    Update card and sum elements

--- Section-3/index.js
+++ Section-3/index.js
@@ -5,13 +5,18 @@
 
 let firstCard = 10;
 let secondCard = 11;
-let sum = firstCard + secondCard + 4;
+let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
-
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+//let sumEl = document.querySelector("#sum-el"); // element with id sum-el
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
@@ -21,5 +26,5 @@ function startGame() {
         isAlive = false;
         message = "You lose!";
     }
-    console.log(message);
+    messageEl.textContent = message;
 }
