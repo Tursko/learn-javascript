7e0df8f5ad36abec0d2d9fde07b0fd10b7dbb3cb
Tursko <taylor@stykocompany.com>
2023-01-08 19:08:05 -0600

    start section 4 exercises

---
+++ Section-4/index.js
@@ -0,0 +1,12 @@
+console.log("-First exercise-");
+let person = {
+    name: "person",
+    age: 30,
+    country: "USA"
+}
+
+function logData() {
+    let message = person.name + " is " + 30 +  " years old and lives in the " + person.country;
+    console.log(message);
+}
+logData();
