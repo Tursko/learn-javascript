4d3459874c46ccf6c3fc3c95a7745c41705475e6
Tursko <taylor@stykocompany.com>
2023-01-08 18:59:41 -0600

    complete basic functions of the game

--- Section-3/index.js
+++ Section-3/index.js
@@ -1,3 +1,11 @@
+let player = {
+    name: "Person",
+    chips: 145,
+    sayHello: function () {
+        console.log("Hello my name is " + this.name);
+    }
+}
+
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -7,11 +15,6 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 //let sumEl = document.querySelector("#sum-el"); // element with id sum-el
 let cardsEl = document.getElementById("cards-el");
-
-let player = {
-    name: "Person",
-    chips: 145
-}
 
 let playerEl = document.getElementById("player-el");
 playerEl.textContent = player.name + ": $" + player.chips;
@@ -29,6 +32,9 @@ function getRandomCard() {
 
 function startGame() {
     isAlive = true;
+    hasBlackJack = false;
+    cards = [];
+    sum = 0;
 
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
@@ -38,11 +44,16 @@ function startGame() {
     renderGame();
 }
 
+
 function renderGame() {
     // Update list of cards
     cardsEl.textContent = "Cards: ";
     for(let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " ";
+        if (i != cards.length-1) {
+            cardsEl.textContent += cards[i] + ", ";
+        } else {
+            cardsEl.textContent += cards[i];
+        }
     }
 
     sumEl.textContent = "Sum: " + sum;
@@ -50,10 +61,10 @@ function renderGame() {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
         hasBlackJack = true;
-        message = "BlackJack! You win!";
+        message = "BlackJack! You win! Start a new game.";
     } else {
         isAlive = false;
-        message = "You lose!";
+        message = "You lose! Start a new game.";
     }
     messageEl.textContent = message;
 }
