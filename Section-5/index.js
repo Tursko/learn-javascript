7d6efd5e004344b5b5ac7f09b3b333438c32d668
Tursko <taylor@stykocompany.com>
2023-01-10 21:51:32 -0600

    render list on the page

--- Section-5/index.js
+++ Section-5/index.js
@@ -1,10 +1,26 @@
 let myEntries = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
 inputBtn.addEventListener("click", function() {
-    if (inputEl.value !== ""){
+    if (inputEl.value !== "") {
         myEntries.push(inputEl.value);
         inputEl.value = "";
         console.log(myEntries);
     }
-})
+
+    renderEntries();
+});
+
+function renderEntries() {
+    let listItems = "";
+    for (let i = 0; i < myEntries.length; i++) {
+        // Alternative solution
+        //const listItem = document.createElement("li");
+        //listItem.textContent = myEntries[i];
+        //ulEl.append(listItem);
+        listItems += "<li>" + myEntries[i] + "</li>";
+    }
+    ulEl.innerHTML = listItems;
+}
