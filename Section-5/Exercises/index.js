38bf887897ba37b11c880ac8e96e73ccecefaa3a
Tursko <taylor@stykocompany.com>
2023-01-16 20:53:13 -0600

    converting and parsing JSON

--- Section-5/Exercises/index.js
+++ Section-5/Exercises/index.js
@@ -3,3 +3,17 @@ localStorage.setItem("link1", "google.com");
 console.log(localStorage.getItem("link1"));
 
 localStorage.clear();
+
+// Store array in local storage by converting to JSON
+let array = ["something1", "something2", "something3"];
+localStorage.setItem("array", JSON.stringify(array));
+let array2 = JSON.parse(localStorage.getItem("array"));
+console.log(array2);
+
+// More practice converting, pushing, reconverting
+let myArrayString = `["entry1", "entry2"]`;
+let myArray = JSON.parse(myArrayString);
+myArray.push("entry3");
+myArrayString = JSON.stringify(myArray);
+console.log(typeof myArrayString);
+console.log(myArrayString);
--- Section-5/index.js
+++ Section-5/index.js
@@ -1,4 +1,5 @@
 let myEntries = [];
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
