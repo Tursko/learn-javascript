8c550cbdf24b3ba1103c7d5c7657aee3fb3f1d77
Tursko <taylor@stykocompany.com>
2023-01-30 13:52:13 -0600

    Add delete all functionality

--- Section-5/index.js
+++ Section-5/index.js
@@ -1,14 +1,29 @@
 let myEntries = [];
+const entriesName = "myEntires";
 
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
+
+const localStorageEntries = JSON.parse(localStorage.getItem(entriesName));
+ if (localStorageEntries) {
+    myEntries = localStorageEntries;
+    renderEntries();
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.removeItem(entriesName);
+    myEntries = [];
+    renderEntries();
+})
 
 inputBtn.addEventListener("click", function() {
     if (inputEl.value !== "") {
         myEntries.push(inputEl.value);
         inputEl.value = "";
-        console.log(myEntries);
+        // Convert to JSON and store in localStorage
+        localStorage.setItem(entriesName, JSON.stringify(myEntries));
     }
 
     renderEntries();
